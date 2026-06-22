6b032491aefa1434eb8f9fad2681f2ae85147fae
Aastha Sapate <aasthasapate4@gmail.com>
2026-06-23 00:29:05 +0530

    Removed unnecessary files

--- python.py
+++
@@ -1,7 +0,0 @@
-import streamlit as st
-st.title("Nassau Candy Dashboard")
-
-st.metric("Total Orders", total_orders)
-st.metric("Total Sales", round(total_sales))
-st.metric("Total Profit", round(total_profit))
-st.metric("Avg Lead Time", round(avg_lead_time, 2))
